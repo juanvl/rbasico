752cdae49a13d7e6dc8d1386f0548cfc41a331c6
Juan <jnvleite@gmail.com>
2019-12-20 20:21:19 -0300

    import helpers eslint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import './config/ReactotronConfig';
-import GlobalStyles from 'styles/global';
-import Routes from './routes';
+import '~/config/ReactotronConfig';
+import Routes from '~/routes';
+import GlobalStyles from '~/styles/global';
 
 const App = () => (
   <>
